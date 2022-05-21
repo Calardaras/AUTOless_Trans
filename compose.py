3eaa94e937f3ff46c90f53366851e06e04eb6202
Calardaras <Karny_Ebony@outlook.com>
2022-05-21 21:23:25 +0800

    0.0.2 子目录遍历

--- compose.py
+++ compose.py
@@ -5,23 +5,34 @@ path_org = "original\\english"
 path_otp = "output\\simp_chinese"
 path_tmp = "process"
 
+def file_search_to_list(path):
+    L = []
+    for root,dirs,files in os.walk(path):
+        for file in files:
+            all_file_path = os.path.join(root,file)
+            file_name_and_format = os.path.splitext(all_file_path)
+            if file_name_and_format[1] == '.yml':
+                L.append(all_file_path)
+    return L
+
+
 # 创建中间文件
-key_f = open( path_tmp + '\\' + 'key.txt' ,'w+',encoding="utf-8") 
-vaule_f = open( path_tmp + '\\' + 'vaule.txt' ,'w+',encoding="utf-8") 
-contents_f = open( path_tmp + '\\' + 'contents.txt' ,'w+',encoding="utf-8") 
+key_f = open( path_tmp + '\\' + 'key.txt' ,'w+',encoding="utf-8-sig") 
+vaule_f = open( path_tmp + '\\' + 'vaule.txt' ,'w+',encoding="utf-8-sig") 
+contents_f = open( path_tmp + '\\' + 'contents.txt' ,'w+',encoding="utf-8-sig") 
 # 读取原始文件
 # # os.listdir()方法获取文件夹名字，返回数组
-file_name_list = os.listdir(path_org)
+file_name_list = file_search_to_list(path_org)
 index = 1 
 for crr_file in file_name_list:
-    org_f = open( path_org + '\\' + crr_file ,'r',encoding="utf-8") 
+    org_f = open( crr_file ,'r',encoding="utf-8-sig") 
     next(org_f) #跳过首行
     for line in org_f:  #通过迭代器访问
         if re.search('^( ?)#.+',line):
             continue
         else:
-            if re.search('^( ?)[^#][^:]+?:[01]?',line):
-                key_f.write(re.search('^( ?)[^#][^:]+?:[01]?',line).group(0)+'\n')
+            if re.search('^( ?)[^#][^:]+?:\d?',line):
+                key_f.write(re.search('^( ?)[^#][^:]+?:\d?',line).group(0)+'\n')
                 vaule_f.write(re.search('(".*")',line).group(0)+'\n')
                 index += 1
     contents_f.write(crr_file+','+str(index)+'\n')
--- decompose.py
+++ decompose.py
@@ -6,10 +6,10 @@ path_otp = "output\\simp_chinese"
 path_tmp = "process"
 
 # 读取中间文件
-key_f = open( path_tmp + '\\' + 'key.txt' ,'r',encoding="utf-8") 
+key_f = open( path_tmp + '\\' + 'key.txt' ,'r',encoding="utf-8-sig") 
 key_l = list(key_f)
 key_f.close()
-contents_f = open( path_tmp + '\\' + 'contents.txt' ,'r',encoding="utf-8") 
+contents_f = open( path_tmp + '\\' + 'contents.txt' ,'r',encoding="utf-8-sig") 
 
 
 #处理目录和引索
@@ -22,13 +22,13 @@ contents_f.close()
 #拆分
 
 # for content in contents_f:
-#     curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', content).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+#     curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', content).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8-sig") 
 #     curr_f.write('l_simp_chinese:')
 #     end = re.search('[^,]+$', content).group(0)
 document = Document(path_tmp + '\\' + 'tr_va.docx')
 i = 0
 j = 0
-curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8-sig") 
 curr_f.write('l_simp_chinese:\n')
 for paragraph in document.paragraphs:
     if j< int(index[i])-1:
@@ -37,5 +37,5 @@ for paragraph in document.paragraphs:
     else:
         curr_f.close()
         i+=1
-        curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+        curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8-sig") 
         curr_f.write('l_simp_chinese:\n')
