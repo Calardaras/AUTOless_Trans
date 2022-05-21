7eea9cbf72d57da78c641612b47360f8a3b80624
Calardaras <Karny_Ebony@outlook.com>
2022-05-21 09:25:07 +0800

    0.0.1

--- resemble.py
+++ resemble.py
@@ -7,12 +7,36 @@ path_tmp = "process"
 
 # 读取中间文件
 key_f = open( path_tmp + '\\' + 'key.txt' ,'r',encoding="utf-8") 
-index_f = open( path_tmp + '\\' + 'index.txt' ,'r',encoding="utf-8") 
+key_l = list(key_f)
+key_f.close()
+contents_f = open( path_tmp + '\\' + 'contents.txt' ,'r',encoding="utf-8") 
 
-for indexes in index_f:
-    curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', indexes).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
-    curr_f.write('l_simp_chinese:')
-# document = Document(path_tmp + '\\' + 'tr_va.docx')
-# for paragraph in document.paragraphs:
-#     print(paragraph.text)
+
+#处理目录和引索
+file_name =[]
+index = []
+for content in contents_f:
+    file_name.append(re.search('^[^,]+', content).group(0))
+    index.append(re.search('[^,]+$', content).group(0))
+contents_f.close()
+#拆分
+
+# for content in contents_f:
+#     curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', content).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+#     curr_f.write('l_simp_chinese:')
+#     end = re.search('[^,]+$', content).group(0)
+document = Document(path_tmp + '\\' + 'tr_va.docx')
+i = 0
+j = 0
+curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+curr_f.write('l_simp_chinese:\n')
+for paragraph in document.paragraphs:
+    if j< int(index[i]):
+        curr_f.write(re.search('^[^(\n)]+',key_l[i]).group(0) +' '+ paragraph.text +'\n')
+        j+=1
+    else:
+        curr_f = open( path_otp  + '\\' + re.search('.+(?=(_l_english))',re.search('^[^,]+', file_name[i]).group(0)).group(0) + '_l_simp_chinese.yml','w+',encoding="utf-8") 
+        curr_f.write('l_simp_chinese:\n')
+        i+=1
+
 
