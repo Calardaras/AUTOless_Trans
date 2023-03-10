72f6e33bcf05fe862c74ed6a40f220a274de0b36
Calardaras <Karny_Ebony@Outlook.com>
2023-03-11 01:17:03 +0800

    new file:   .gitignore 	modified:   rename.py

--- rename.py
+++ rename.py
@@ -2,17 +2,15 @@ import os,sys
 import re
 #获取文件列表
 path_org = "original"
-path_otp = "output"
-path_tmp = "process"
-path_bfr = "tobetrans"
-path_aft = "transed"
-path_rnm = "renamed"
-
+filename = ""
 Org_dir = []
-for root,dirs,files in os.walk(path_org):
+all_files = os.walk(path_org)
+for root,dirs,files in all_files:
     for file in files:
         all_file_path = os.path.join(root,file)
         file_name_and_format = os.path.splitext(all_file_path)
         if file_name_and_format[1] == '.yml':
             Org_dir.append(all_file_path)
-            os.renames(all_file_path,re.sub('_english','_simp_chinese',all_file_path))
+            old_name = all_file_path
+            new_name = re.sub('_english','_simp_chinese',all_file_path)
+            os.renames(old_name,new_name)
